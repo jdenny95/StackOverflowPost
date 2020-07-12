c3ed1f2da27f14e0b7062ea687c2f643b78692b8
jdenny95 <51931801+jdenny95@users.noreply.github.com>
2020-07-12 19:46:59 +0100

    Add files via upload

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public readonly DateTime DateCreated = DateTime.UtcNow;
+        public int UpVotesCount { get; private set; }
+        public int DownVotesCount { get; private set; }
+        public int PostScore => UpVotesCount - DownVotesCount;
+
+        public void UpVote()
+        {
+            UpVotesCount++;
+        }
+
+        public void DownVote()
+        {
+            DownVotesCount++;
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post();
+
+            newPost.Title = "How to do I create a post to StackOverflow?";
+            newPost.Description = "I can't post anything to StackOverflow.";
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.DownVote();
+            newPost.UpVote();
+            newPost.UpVote();
+            newPost.UpVote();          
+
+            Console.WriteLine(newPost.Title);
+            Console.WriteLine(newPost.Description);
+            Console.WriteLine(newPost.DateCreated);
+            Console.WriteLine();
+            Console.WriteLine("Upvotes: " + newPost.UpVotesCount);
+            Console.WriteLine("Downvotes: " + newPost.DownVotesCount);
+            Console.WriteLine("Post Score: " + newPost.PostScore);
+        }
+    }
+}
